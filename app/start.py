53e68eb117f1b10b8f0335e67a9c89efa20c88b8
Sher <sherjunior6505@gmail.com>
2017-12-20 13:35:50 -0500

    user session issue is solved

--- app/start.py
+++ app/start.py
@@ -15,10 +15,10 @@ def sign():
       status=request.form['options']
       if status=="Refugee":
           user = Refugees.get(Refugees.username==username)
-          return render_template("refugee/profile.html", user=user)
+          return render_template("refugee/profile.html", user=user, cfg=cfg)
       elif status=="Employer":
           user=Employers.get(Employers.username==username)
-          return render_template("employer/profile.html", user=user)
+          return render_template("employer/profil.html", user=user, cfg=cfg)
       else:
           user=Asylums.get(Asylums.username==username)
           return render_template("index.html", user=user)
@@ -26,38 +26,45 @@ def sign():
       return render_template("sign.html")
 
 # Refugee routes
-@app.route("/info", methods=["GET"])
-def info():
-    return render_template("profile.html")
+@app.route("/info/<username>", methods=["GET"])
+def info(username):
+    user = Refugees.get(Refugees.username==username)
+    return render_template("refugee/profile.html", cfg=cfg, user=user)
 
-@app.route("/search", methods=["GET"])
-def search():
-    return render_template("profile.html")
+@app.route("/search/<username>", methods=["GET", "POST"])
+def search(username):
+    user = Refugees.get(Refugees.username==username)
+    return render_template("refugee/search.html", cfg=cfg, user=user)
 
-@app.route("/dashboard", methods=["GET"])
-def dashboard():
-    return render_template("profile.html")
+@app.route("/dashboard/<username>", methods=["GET"])
+def dashboard(username):
+    user = Refugees.get(Refugees.username==username)
+    return render_template("refugee/dashboard.html", cfg=cfg, user=user)
 
-@app.route("/profile", methods=["GET"])
+@app.route("/settings", methods=["GET"])
 def settings():
-    return render_template("profile.html")
+    return render_template("refugee/settings.html")
 
 @app.route("/out", methods=["GET"])
 def out():
     return render_template("index.html")
-#Employer Routes
 
-@app.route("/profile", methods=["GET"])
-def info():
-    return render_template("profile.html")
+
+
+#Employer Routes
+@app.route("/profil/<username>", methods=["GET"])
+def profil(username):
+    user=Employers.get(Employers.username==username)
+    return render_template("employer/profil.html", cfg=cfg, user=user)
 
 @app.route("/postjob", methods=["GET"])
 def postjob():
     return render_template("profile.html")
 
-@app.route("/dash", methods=["GET"])
-def dash():
-    return render_template("profile.html")
+@app.route("/dash/<username>", methods=["GET"])
+def dash(username):
+    user=Employers.get(Employers.username==username)
+    return render_template("employer/dashboard.html", cfg=cfg, user=user)
 
 @app.route("/setting", methods=["GET"])
 def setting():
